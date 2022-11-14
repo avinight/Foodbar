529e03a6ac2a3027151eeaccf9b19f754179957c
Jun-Ars <jun.arshad@gmail.com>
2022-11-14 17:30:40 -0500

    Updated structure for Recipe to seperate it from User

--- usecase/src/main/java/recipes/foodbar/usecase/user/exception/RecipeValidationException.java
+++
@@ -1,7 +0,0 @@
-package recipes.foodbar.usecase.user.exception;
-
-public class RecipeValidationException extends RuntimeException{
-    public RecipeValidationException(final String message) {
-        super(message);
-    }
-}
--- usecase/src/main/java/recipes/foodbar/usecase/user/port/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package recipes.foodbar.usecase.user.port;
-
-import recipes.foodbar.entities.Recipe;
-
-public interface RecipeRepository {
-    Recipe create(Recipe recipe);
-}
