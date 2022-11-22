bcd231e4b7c929b16df233a3a9e1bcb9f2a39f44
Frederick Meneses <75036259+avinight@users.noreply.github.com>
2022-11-21 22:56:35 -0500

    Sorting stuff added so it wont be deleted
    Signed-off-by: Frederick Meneses <75036259+avinight@users.noreply.github.com>

--- src/main/java/recipe/foodbar/usecase/sort/IngredientComparator.java
+++ src/main/java/recipe/foodbar/usecase/sort/IngredientComparator.java
@@ -1,5 +1,6 @@
 package recipe.foodbar.usecase.sort;
 
+import recipe.foodbar.entities.Ingredient;
 import recipe.foodbar.entities.Recipe;
 
 public class IngredientComparator extends AbstractComparator{
@@ -10,6 +11,25 @@ public class IngredientComparator extends AbstractComparator{
      */
     @Override
     public int compare(Recipe r1, Recipe r2) {
-        return 0;
+        String s1 = null;
+        String s2 = null;
+
+        for (Ingredient ingredient : r1.getIngredients()) {
+            if (r1.getTitle().contains(ingredient.getName())) {
+                s1 = ingredient.getName();
+            }
+        }
+
+        for (Ingredient ingredient : r2.getIngredients()) {
+            if (r1.getTitle().contains(ingredient.getName())) {
+                s2 = ingredient.getName();
+            }
+        }
+
+        if(s1 == null || s2 == null) {
+            return String.CASE_INSENSITIVE_ORDER.compare(s1, s2);}
+        else {
+            return String.CASE_INSENSITIVE_ORDER.compare(r1.getTitle(), r2.getTitle());
+        }
     }
 }
