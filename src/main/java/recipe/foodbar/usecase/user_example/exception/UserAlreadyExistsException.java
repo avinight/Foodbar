335db96b9c858821a00e4333dc95727cf92cbdfe
RoneyThomas <roneythomas6@gmail.com>
2022-11-24 18:37:27 -0500

    Removed UserExample

--- src/main/java/recipe/foodbar/usecase/user_example/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package recipe.foodbar.usecase.user_example.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(final String email) {
-        super(email);
-    }
-}
--- src/main/java/recipe/foodbar/usecase/user_example/exception/UserValidationException.java
+++
@@ -1,7 +0,0 @@
-package recipe.foodbar.usecase.user_example.exception;
-
-public class UserValidationException extends RuntimeException {
-    public UserValidationException(final String message) {
-        super(message);
-    }
-}
