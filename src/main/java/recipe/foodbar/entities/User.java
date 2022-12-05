3b52b9d0d80347b4ad6b35f79bed0404ea2209b3
RoneyThomas <roneythomas6@gmail.com>
2022-12-05 18:34:12 -0500

    Added lombok annotations for User entity

--- src/main/java/recipe/foodbar/entities/User.java
+++ src/main/java/recipe/foodbar/entities/User.java
@@ -1,10 +1,16 @@
 package recipe.foodbar.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 
+@Builder
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     private String id;
@@ -16,71 +22,8 @@ public class User {
     private ArrayList<User> following;
     private ArrayList<User> followers;
 
-    public User(final String id, final String username, final String password,
-                final String first, final String last, final String email) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.firstName = first;
-        this.lastName = last;
-        this.email = email;
-        this.following = new ArrayList<User>();
-        this.followers = new ArrayList<User>();
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return this.username;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public ArrayList<User> getFollowing() {
-        return following;
-    }
-
-    public void setFollowing(ArrayList<User> following) {
-        this.following = following;
-    }
-
-    public ArrayList<User> getFollowers() {
-        return followers;
-    }
-
-    public void setFollowers(ArrayList<User> followers) {
-        this.followers = followers;
-    }
-
     @Override
     public String toString() {
-        return "User{" +
-                "id='" + id + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", following='" + following + '\'' +
-                ", followers='" + followers + '\'' +
-                '}';
+        return "User{" + "id='" + id + '\'' + ", username='" + username + '\'' + ", password='" + password + '\'' + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + ", email='" + email + '\'' + ", password='" + password + '\'' + ", following='" + following + '\'' + ", followers='" + followers + '\'' + '}';
     }
-
 }
