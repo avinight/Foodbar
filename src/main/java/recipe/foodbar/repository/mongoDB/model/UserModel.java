1ee59380c67dd0a1ef4ee11bccd15ae0ab215bfe
RoneyThomas <roneythomas6@gmail.com>
2022-12-05 22:09:56 -0500

    Added default value for ArrayList when creating a new object.

--- src/main/java/recipe/foodbar/repository/mongoDB/model/UserModel.java
+++ src/main/java/recipe/foodbar/repository/mongoDB/model/UserModel.java
@@ -1,6 +1,7 @@
 package recipe.foodbar.repository.mongoDB.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
@@ -16,6 +17,9 @@ public class UserModel {
     private String username;
     private String password;
     private String email;
-    private ArrayList<User> following;
-    private ArrayList<User> followers;
+    @Builder.Default
+    private ArrayList<User> following = new ArrayList<>();
+    @Builder.Default
+    private ArrayList<User> followers = new ArrayList<>();
+    ;
 }
