948688a8ca2b8d5a1235f434e70327a04217d541
Frederick Meneses <75036259+avinight@users.noreply.github.com>
2022-12-07 18:22:39 -0500

    Changes
    Signed-off-by: Frederick Meneses <75036259+avinight@users.noreply.github.com>

--- src/main/java/recipe/foodbar/usecase/recipe/port/RecipeRepository.java
+++ src/main/java/recipe/foodbar/usecase/recipe/port/RecipeRepository.java
@@ -1,6 +1,6 @@
 package recipe.foodbar.usecase.recipe.port;
 
-import recipe.foodbar.entities.Cuisine;
+import recipe.foodbar.entities.Cuisine.Cuisine;
 import recipe.foodbar.entities.Recipe;
 
 import java.util.ArrayList;
